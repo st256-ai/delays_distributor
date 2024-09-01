ade7b0f0d0569edebc56b43f6b3ba507bcb8a15c
st256-ai <andrey-krivenko00@yandex.ru>
2024-09-01 20:50:27 +0300

    updated verilog logic

--- phase_shift_process.v
+++ phase_shift_process.v
@@ -12,17 +12,18 @@ localparam [2:0] SHIFT = 3'b001;
 localparam [2:0] VALIDATE = 3'b010;
 localparam [2:0] RESET = 3'b100;
 
-reg [7:0] _procced_periods;
+reg [7:0] _processed_periods;
 reg [2:0] _cnt;
-reg _th_passed;
+reg _phase_done;
 
 reg [2:0] state;
 reg [2:0] next_state;
 
 initial begin
-	_procced_periods <= 8'b00000000;
+	_processed_periods <= 8'b00000000;
 	_cnt <= 3'b000;
-	_th_passed <= 1'b0;
+	_phase_done <= 1'b1;
+	o_phasestep <= 1'b0;
 	
 	state <= RESET;
 	next_state <= RESET;
@@ -31,59 +32,107 @@ end
 always @(negedge i_clk)
 	state <= next_state;
   
-always @(negedge i_clk) begin
+always @(*) begin
 	case(state)
 		LISTEN: 
 			next_state <= i_ready && PLL_NUM == i_pll_to_update ? SHIFT : LISTEN;
 		SHIFT:
-			next_state <= _th_passed && i_phasedone ? VALIDATE : SHIFT;
+			next_state <= _cnt >= 3'b001 && !_phase_done ? VALIDATE : SHIFT;
 		VALIDATE: 
-			next_state <= _procced_periods >= i_periods_to_process ? RESET : SHIFT;
+			next_state <= _processed_periods >= i_periods_to_process ? RESET : SHIFT;
 		RESET: 
 			next_state <= LISTEN;
    endcase
 end
 
-always @(*) begin
+always @(negedge i_clk) begin
 	case(state)
 		LISTEN: begin
-			_cnt <= 3'b000;
-			_th_passed <= 1'b0;
-			o_phasestep <= 1'b0;
+			_cnt = 3'b000;
+			o_phasestep = 1'b0;
 		end
 		SHIFT: begin
-			o_phasestep <= 1'b1;
-			if (_cnt == 2)
-				_th_passed = 1'b1;
+			_cnt = _cnt + 1'b1;
+			if (_cnt == 1) begin
+				o_phasestep = 1'b1;
+				_processed_periods = _processed_periods + 1'b1;
+			end
+			
+			if (_cnt > 2) begin
+				o_phasestep = 1'b0;
+			end
+			
+			if (!i_phasedone) begin
+				_phase_done = 1'b0;
+				//if(_cnt >= 1) begin
+				//	o_phasestep <= 1'b0;
+				//end
+			end
 		end
 		VALIDATE: begin
-			_procced_periods <= _procced_periods + 1'b1;
-		
-			_cnt <= 3'b000;
-			_th_passed <= 1'b0;
-			o_phasestep <= 1'b0;
+			_cnt = 3'b000;
+			_phase_done = 1'b1;
+			o_phasestep = 1'b0;
 		end
 		RESET: begin
-			_procced_periods <= 8'b00000000;
+			_processed_periods = 8'b00000000;
 		end
    endcase
 end
 
 endmodule
 
-//testbench
-
-//testscale 1ns/1ns
-
+//testbench 1ns/1ns
 module phase_shift_testbench();
+
+	localparam [7:0] INITIAL_PERIODS_NUM = 8'b00000101;
+
 	reg i_clk;
-	reg [7:0] i_periods_to_process,;
+	reg [7:0] i_periods_to_process;
 	reg i_phasedone;
 	reg i_ready;
 	reg i_pll_to_update;
 	wire o_phasestep;
 	
-	initial begin
-		
+	integer i;
+	                        
+	phase_shift_processor phase_shift_processor (
+       .i_clk(i_clk),
+		 .i_periods_to_process(i_periods_to_process),
+		 .i_phasedone(i_phasedone),
+		 .i_ready(i_ready),
+		 .i_pll_to_update(i_pll_to_update),
+		 .o_phasestep(o_phasestep)
+	);
 
-end
+	initial begin                                                   
+		i_clk <= 1'b0;
+		i_periods_to_process <= INITIAL_PERIODS_NUM;
+		i_phasedone <= 1'b1; 
+		i_ready <= 1'b0;
+ 		i_pll_to_update <= 1'b0;                       
+	end
+
+	always
+		#20  i_clk = !i_clk;
+
+	always begin
+		#75; 
+		i_ready <= 1'b1;
+		#40;
+		i_ready <= 1'b0;
+		
+		if(i_pll_to_update == 1'b0) begin
+			for(i=0; i < i_periods_to_process; i=i+1) begin
+				#200;
+				i_phasedone <= 1'b0; #60;
+				i_phasedone <= 1'b1;
+			end
+		end
+		
+		//i_pll_to_update <= i_periods_to_process % 2;
+		#600;
+		i_periods_to_process <= i_periods_to_process + 1;
+	end
+ 
+endmodule
--- top.v
+++ top.v
@@ -3,7 +3,8 @@ module top(
 	input wire i_uart_rx,					 // input port for UART reciever
 	output wire [7:0] o_modulating, 		 // modulating signals for chaotic generators
 	output wire o_uart_tx,					 // output port for UART transmitter 
-	output wire [2:0] o_current_byte_num
+	output wire [2:0] o_current_byte_num,
+	output wire o_phaseupdown
 );
 
 
@@ -25,9 +26,8 @@ wire rxReady;
 wire wc0,wc1,wc2,wc3;  	 	// PLL output
 reg areset_1;					// async reset (not used here) 
 wire [2:0] phasecounter_1;	// counters assosiated with PLL outputs
-reg phaseupdown_1;			// direction of phase shifting
-wire phasestep_1;           // signal to enable phase shift
-wire phasedone_1;          // shows when current shift is finished (if it's value == 0 => current pahse shift have been done)
+wire phasestep_1;          // signal to enable phase shift
+wire phasedone_1;          // shows when current shift is finished (if it's value == 0 => current phase shift have been done)
 wire locked_1;					// PLL locked output
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -35,11 +35,12 @@ wire locked_1;					// PLL locked output
 wire wc4,wc5,wc6,wc7; 	 	// PLL output
 reg areset_2;				 	// async reset (not used here) 
 wire [2:0] phasecounter_2;	// counters assosiated with PLL outputs
-reg phaseupdown_2;         // direction of phase shifting
-wire phasestep_2;           // signal to enable phase shift
-wire phasedone_2;          // shows when current shift is finished (if it's value == 0 => current pahse shift have been done)
+wire phasestep_2;          // signal to enable phase shift
+wire phasedone_2;          // shows when current shift is finished (if it's value == 0 => current phase shift have been done)
 wire locked_2;					// PLL locked output
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+wire phaseupdown; 			// direction of phase shifting
+
 wire [7:0] periods_to_process;
 wire pll_to_update;
 wire shift_ready;
@@ -47,14 +48,13 @@ wire shift_ready;
 initial begin
 	areset_1 <= 1'b0;
 	areset_2 <= 1'b0;
-	
-	phaseupdown_1 <= 1'b1;
-	phaseupdown_2 <= 1'b1;
 end
+
+assign o_phaseupdown = phaseupdown;
 
 UART uart
 (
-	.clockIN(clockIN),
+	.clockIN(i_clk),
 	
 	.nTxResetIN(txReset),
 	.txDataIN(txData),
@@ -74,7 +74,7 @@ pll_0_3 pll_0_3(
 	.inclk0(i_clk),
 	.areset(areset_1),
 	.phasecounterselect(phasecounter_1),
-	.phaseupdown(phaseupdown_1),
+	.phaseupdown(phaseupdown),
 	.phasestep(phasestep_1),
 	.scanclk(wc0),
 	.c0(wc0),
@@ -89,7 +89,7 @@ pll_4_7 pll_4_7(
 	.inclk0(i_clk),
 	.areset(areset_2),
 	.phasecounterselect(phasecounter_2),
-	.phaseupdown(phaseupdown_2),
+	.phaseupdown(phaseupdown),
 	.phasestep(phasestep_2),
 	.scanclk(wc4),
 	.c0(wc4),
@@ -104,11 +104,12 @@ uart_data_mapper uart_data_mapper(
 	.i_clk(i_clk),
 	.i_rx_data(rxData),
 	.i_rx_ready(rxReady),
-	.o_current_byte_num(),
 	.o_periods_to_process(periods_to_process),
 	.o_phasecounterselect_1(phasecounter_1),
 	.o_phasecounterselect_2(phasecounter_2),
+	.o_current_byte_num(o_current_byte_num),
 	.o_pll_to_update(pll_to_update),
+	.o_phaseupdown(phaseupdown),
 	.o_shift_ready(shift_ready)
 );
 
@@ -139,4 +140,107 @@ assign o_modulating[5] = !wc5;
 assign o_modulating[6] = !wc6;
 assign o_modulating[7] = !wc7;
 
+endmodule
+
+
+//testbench 1ns/1ns
+module uart_shift_processor_integration_tb();
+
+	localparam [7:0] INITIAL_PERIODS_NUM = 8'b00110101;
+	localparam [7:0] BASE = 8'b00110000;
+	localparam [7:0] STOP_SIGNAL = 8'b01010011;
+
+	reg clk;
+	reg [7:0] i_rx_data;
+	reg i_rx_ready;
+	wire o_current_byte_num;
+	wire [7:0] periods_to_process;
+	wire [2:0] phasecounterselect_1;
+	wire [2:0] phasecounterselect_2;
+	wire pll_to_update;
+	wire o_phaseupdown;
+	wire shift_ready;
+	
+	reg [7:0] _counter;
+	reg [7:0] _gener_num_counter;
+	reg i_phasedone;
+	wire o_phasestep;
+	
+	integer i;
+	                        
+	uart_data_mapper uart_data_mapper(
+       .i_clk(clk),
+		 .i_rx_data(i_rx_data),
+	    .i_rx_ready(i_rx_ready),
+	    .o_current_byte_num(o_current_byte_num),
+	    .o_periods_to_process(periods_to_process),
+	    .o_phasecounterselect_1(phasecounterselect_1),
+	    .o_phasecounterselect_2(phasecounterselect_2),
+		 .o_phaseupdown(o_phaseupdown),
+	    .o_pll_to_update(pll_to_update),
+	    .o_shift_ready(shift_ready)
+	);
+	
+	phase_shift_processor phase_shift_processor (
+       .i_clk(clk),
+		 .i_periods_to_process(periods_to_process),
+		 .i_phasedone(i_phasedone),
+		 .i_ready(shift_ready),
+		 .i_pll_to_update(pll_to_update),
+		 .o_phasestep(o_phasestep)
+	);
+
+	initial begin                                                   
+		clk <= 1'b0;
+		_counter <= BASE;
+		_gener_num_counter <= BASE;
+		i_phasedone <= 1'b1;
+		i_rx_data <= INITIAL_PERIODS_NUM;
+		i_rx_ready <= 1'b0;
+		i <= 0;
+	end
+
+	always 
+		#20  clk = !clk;  
+
+	always begin
+		//Sending generator's number
+		#120;
+		i_rx_ready <= 1'b1;
+		_gener_num_counter <= _counter & 7;
+		i_rx_data <= BASE + _gener_num_counter;
+		#40;
+		i_rx_ready <= 1'b0;
+		
+		//Sending flag for shift direction determining
+		#120;
+		i_rx_data <= BASE;
+		i_rx_ready <= 1'b1;
+		#40;
+		i_rx_ready <= 1'b0;
+		
+		//Sending number of shifts to be performed
+		#320;
+		i_rx_ready <= 1'b1;
+		i_rx_data <= _counter;
+		#40;
+		i_rx_ready <= 1'b0;
+		
+		//Sending STOP_SIGNAL
+		#320;
+		i_rx_ready <= 1'b1;
+		i_rx_data <= STOP_SIGNAL;
+		#40;
+		i_rx_ready <= 1'b0;
+		
+		for(i=0; i < periods_to_process; i=i+1) begin
+			#140;
+			i_phasedone <= 1'b0; #60;
+			i_phasedone <= 1'b1;
+		end
+		
+		_counter <= _counter + 1'b1;
+		#320;
+	end  
+ 
 endmodule
